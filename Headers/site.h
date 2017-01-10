b07d36e2688e6f2a3996442b9e0c9b58c33536ea
Red_Menace <ivanderlich@gmail.com>
2017-01-10 16:06:03 -0300

    avanzando

---
+++ Headers/site.h
@@ -0,0 +1,14 @@
+#ifndef __GETPOST_H__
+#define __GETPOST_H__
+
+using namespace std;
+
+class Site{
+	private:
+	protected:
+	public:
+		void inicio();
+		void prueba();
+}
+
+#endif /*__GETPOST_H__*/
